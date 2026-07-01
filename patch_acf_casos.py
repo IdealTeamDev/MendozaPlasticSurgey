598defb8f22e14180aa21b741cef26734a20bcd3
Demoncornio <user@iMac-de-user.local>
2026-07-01 03:17:14 -0500

    style: update faq font to avenir

---
+++ patch_acf_casos.py
@@ -0,0 +1,52 @@
+import json
+import uuid
+
+group_key = "group_casos_clinicos"
+
+acf_group = {
+    "key": group_key,
+    "title": "Campos: Casos Cl\u00ednicos",
+    "fields": [
+        {
+            "key": "field_foto_antes",
+            "label": "Foto Antes",
+            "name": "foto_antes",
+            "type": "image",
+            "return_format": "url",
+            "preview_size": "medium",
+            "library": "all"
+        },
+        {
+            "key": "field_foto_despues",
+            "label": "Foto Despu\u00e9s",
+            "name": "foto_despues",
+            "type": "image",
+            "return_format": "url",
+            "preview_size": "medium",
+            "library": "all"
+        }
+    ],
+    "location": [
+        [
+            {
+                "param": "post_type",
+                "operator": "==",
+                "value": "caso"
+            }
+        ]
+    ],
+    "menu_order": 0,
+    "position": "normal",
+    "style": "default",
+    "label_placement": "top",
+    "instruction_placement": "label",
+    "hide_on_screen": "",
+    "active": True,
+    "description": "",
+    "show_in_rest": 1
+}
+
+output = [acf_group]
+
+with open("acf-parche-casos.json", "w") as f:
+    json.dump(output, f, indent=4)
