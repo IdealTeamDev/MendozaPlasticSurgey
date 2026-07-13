5489814b0e5f11790338a087e0b98e83c1ba79f9
Demoncornio <user@iMac-de-user.local>
2026-07-13 18:20:05 -0500

    Remove icon overlay from ProcedureIntro

--- src/components/procedimientos/ProcedureIntro.tsx
+++ src/components/procedimientos/ProcedureIntro.tsx
@@ -62,12 +62,6 @@ export default function ProcedureIntro({
                 </div>
               )}
             </div>
-            <div className="proc-intro-icon-overlay">
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-              </svg>
-            </div>
           </div>
 
         </div>
